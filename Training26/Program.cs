be31b79d1cbeaf0f216dad314da7125ad54a5db3
SuvarajMetamation <Suvaraj.Balakrishnan@trumpf.com>
2026-07-16 12:07:41 +0530

    Resolve

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Training26;
 
-public static class Program {
+class Program {
    static void Main (string[] args) {
       Console.WriteLine(BracketChecker.IsBracketChecker ("([])")); //True
       Console.WriteLine (BracketChecker.IsBracketChecker ("(]")); //False
