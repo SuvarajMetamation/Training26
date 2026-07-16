4fd303e99dd16b3fe9a2aa5bd48e953bb740fea1
SuvarajMetamation <Suvaraj.Balakrishnan@trumpf.com>
2026-07-16 11:32:05 +0530

    Bracket Checking

---
+++ Training26/BracketChecker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training26 {
+   public static class BracketChecker {
+      public static bool IsBracketChecker (string s) {
+         Stack<char> stack = new Stack<char> ();
+         foreach (char c in s) {
+            if (c == '(' || c == '[') {
+               stack.Push (c);
+            } else if (c == ')' || c == ']') {
+               if (stack.Count == 0)
+                  return false;
+               char top = stack.Pop ();
+               if ((c == ')' && top != '(') ||
+                   (c == ']' && top != '[')) {
+                  return false;
+               }
+            }
+         }
+         return stack.Count == 0;
+      }
+   }
+}
--- Training26/Program.cs
+++ Training26/Program.cs
@@ -2,25 +2,8 @@
 
 public static class Program {
    static void Main (string[] args) {
-      Console.WriteLine(IsBracketChecker ("([])")); //True
-      Console.WriteLine (IsBracketChecker ("(]")); //False
-   }
-   public static bool IsBracketChecker(string s) {
-      Stack<char> stack = new Stack<char> ();
-      foreach (char c in s) {
-         if (c == '(' || c == '[') {
-            stack.Push (c);
-         } else if (c == ')' || c == ']') {
-            if (stack.Count == 0)
-               return false;
-            char top = stack.Pop ();
-            if ((c == ')' && top != '(') ||
-                (c == ']' && top != '[')) {
-               return false;
-            }
-         }
-      }
-      return stack.Count == 0;
+      Console.WriteLine(BracketChecker.IsBracketChecker ("([])")); //True
+      Console.WriteLine (BracketChecker.IsBracketChecker ("(]")); //False
    }
 }
 
