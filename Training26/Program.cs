493729b7f0fc547258533fd5110caa281cd4a6e0
SuvarajMetamation <Suvaraj.Balakrishnan@trumpf.com>
2026-07-16 10:16:35 +0530

    Balanced Bracket Checker

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -1,7 +1,26 @@
 ﻿namespace Training26;
 
-class Program {
+public static class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      Console.WriteLine(IsBracketChecker ("([])")); //True
+      Console.WriteLine (IsBracketChecker ("(]")); //False
+   }
+   public static bool IsBracketChecker(string s) {
+      Stack<char> stack = new Stack<char> ();
+      foreach (char c in s) {
+         if (c == '(' || c == '[') {
+            stack.Push (c);
+         } else if (c == ')' || c == ']') {
+            if (stack.Count == 0)
+               return false;
+            char top = stack.Pop ();
+            if ((c == ')' && top != '(') ||
+                (c == ']' && top != '[')) {
+               return false;
+            }
+         }
+      }
+      return stack.Count == 0;
    }
 }
+
